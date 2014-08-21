201b9467f5934d9d06ba874b6ba2e6e7a08793bb
Guillo96 <srguille@gmail.com>
2014-08-21 14:10:37 -0500

    prieuba

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/franciscolopezhernandez/Documents/BancaALaMano/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/Users/franciscolopezhernandez/Documents/BancaALaMano/platforms/android/CordovaLib/AndroidManifest.xml \
